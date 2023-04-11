3e9a8e3b49dc3c7ccc8b8004b36446434d6a587f
jay bhogayata <jaybhogayata53@gmail.com>
2023-04-11 09:28:55 +0530

    add coupon schema

---
+++ src/models/coupon.schema.js
@@ -0,0 +1,21 @@
+import mongoose from "mongoose";
+
+const couponSchema = new mongoose.Schema(
+  {
+    code: {
+      type: String,
+      required: ["true", "please provide a coupon name"],
+    },
+    discount: {
+      type: Number,
+      default: 0,
+    },
+    active: {
+      type: Boolean,
+      default: true,
+    },
+  },
+  { timestamps: true }
+);
+
+export default mongoose.model("Coupon", couponSchema);
