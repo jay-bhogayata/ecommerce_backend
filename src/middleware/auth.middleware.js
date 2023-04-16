3761412afc6a1a81b0b7ee0013636213205c2ae2
jay bhogayata <jaybhogayata53@gmail.com>
2023-04-16 22:15:07 +0530

    write isLoggedIn middleware

---
+++ src/middleware/auth.middleware.js
@@ -0,0 +1,26 @@
+import User from "../models/user.schema.js";
+import jwt from "jsonwebtoken";
+import asyncHandler from "../utils/asyncHandler.js";
+import CustomError from "../utils/CustomError.js";
+import config from "../config/index.js";
+
+export const isLoggedIn = asyncHandler(async (req, res, next) => {
+  let token;
+  if (
+    req.cookies.token ||
+    (req.headers.authorization &&
+      req.headers.authorization.startsWith("Bearer"))
+  ) {
+    token = req.cookies.token || req.headers.authorization.split(" ")[1];
+  }
+  if (!token) {
+    throw new CustomError("Not authorized to access this resource", 401);
+  }
+  try {
+    const decodedJwtPayload = jwt.verify(token, config.JWT_SECRET);
+    req.user = await User.findById(decodedJwtPayload._id, "name email role");
+    next();
+  } catch (error) {
+    throw new CustomError("Not authorize to access this resource", 401);
+  }
+});
