83838aff88000254a01c4015b3212ea0d43bc3a0
jay bhogayata <jaybhogayata53@gmail.com>
2023-04-18 09:45:34 +0530

    setup logger

--- src/app.js
+++ src/app.js
@@ -2,9 +2,10 @@ import express from "express";
 import cors from "cors";
 import cookieParser from "cookie-parser";
 import routes from "./routes/index.js";
+import morgan from "morgan";
 
 const app = express();
-
+app.use(morgan("dev"));
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 app.use(cors());
