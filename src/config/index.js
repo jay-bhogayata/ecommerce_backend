28e7abdf25e66bd83402ce1618203e4263c8b1c3
jay bhogayata <jaybhogayata53@gmail.com>
2023-04-18 07:26:01 +0530

    add s3 image upload services

--- src/config/index.js
+++ src/config/index.js
@@ -6,6 +6,10 @@ const config = {
   MONGODB_URI: process.env.MONGODB_URI || "mongodb://127.0.0.1:27017/ecommdb",
   JWT_SECRET: process.env.JWT_SECRET || "highlySecret",
   JWT_EXPIRY: process.env.JWT_EXPIRY || "30d",
+  S3_ACCESS_KEY: process.env.S3_ACCESS_KEY,
+  S3_SECRET_ACCESS_KEY: process.env.S3_SECRET_ACCESS_KEY,
+  S3_BUCKET_NAME: process.env.S3_BUCKET_NAME,
+  S3_REGION: process.env.S3_REGION,
 };
 
 export default config;
