ea326239fca37eb65529d266b465991ef2945282
jay bhogayata <jaybhogayata53@gmail.com>
2023-04-20 20:08:00 +0530

    some refactoring is coupon routes

--- src/routes/coupon.routes.js
+++ src/routes/coupon.routes.js
@@ -6,24 +6,13 @@ import {
   getAllActiveCoupons,
   getAllCoupons,
 } from "../controllers/coupon.controllers.js";
-import { authorize, isLoggedIn } from "../middleware/auth.middleware.js";
 
 const router = Router();
 
-router.post("/addCoupon", isLoggedIn, authorize("ADMIN"), addCoupon);
-router.post("/deleteCoupon/:id", isLoggedIn, authorize("ADMIN"), deleteCoupon);
-router.get("/getAllCoupons", isLoggedIn, authorize("ADMIN"), getAllCoupons);
-router.get(
-  "/getAllActiveCoupons",
-  isLoggedIn,
-  authorize("ADMIN"),
-  getAllActiveCoupons
-);
-router.post(
-  "/disableCoupon/:id",
-  isLoggedIn,
-  authorize("ADMIN"),
-  disableCoupon
-);
+router.post("/addCoupon", addCoupon);
+router.post("/deleteCoupon/:id", deleteCoupon);
+router.get("/getAllCoupons", getAllCoupons);
+router.get("/getAllActiveCoupons", getAllActiveCoupons);
+router.post("/disableCoupon/:id", disableCoupon);
 
 export default router;
--- src/routes/index.js
+++ src/routes/index.js
@@ -3,11 +3,12 @@ import authRoutes from "./auth.routes.js";
 import collectionRoutes from "./collection.routes.js";
 import productRoutes from "./product.routes.js";
 import CouponRoutes from "./coupon.routes.js";
+import { authorize, isLoggedIn } from "../middleware/auth.middleware.js";
 
 const router = Router();
 router.use("/auth", authRoutes);
 router.use("/collection", collectionRoutes);
 router.use("/product", productRoutes);
-router.use("/coupon", CouponRoutes);
+router.use("/coupon", isLoggedIn, authorize("ADMIN"), CouponRoutes);
 
 export default router;
